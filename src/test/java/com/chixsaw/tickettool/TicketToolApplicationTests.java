05ac62fb74f457d3a335fda5e9937f5f13e01f81
RaphaelFrey <raphael.marius.frey@gmail.com>
2022-12-04 20:21:05 +0100

    Initial Commit

---
+++ src/test/java/com/chixsaw/tickettool/TicketToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chixsaw.tickettool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
